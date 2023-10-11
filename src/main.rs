a666b571bd4dbd7717b8092a4bd39965777d0363
ash <>
2023-10-11 11:20:19 -0600

    basic drawing, naming in emu.rs

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use ggez::conf::{WindowMode, WindowSetup};
 use ggez::event::{self, EventHandler};
-use ggez::graphics::{self, Color};
+use ggez::graphics::{self, Canvas, Color, DrawParam, Mesh};
 use ggez::{Context, ContextBuilder, GameResult};
 
 mod emu;
@@ -28,13 +28,34 @@ fn main() {
 
 struct MainState {
     emulator: emu::Emu,
+    squares: Vec<Mesh>, // the actually drawn representation of pixels
 }
 
 impl MainState {
-    pub fn new(_ctx: &mut Context) -> MainState {
+    pub fn new(ctx: &mut Context) -> MainState {
         // Load/create resources such as images here.
+        let mut squares: Vec<Mesh> = vec![];
+        for i in 0..32 {
+            for j in 0..64 {
+                let x = (j * 15) as f32;
+                let y = (i * 15) as f32;
+                let rectangle = Mesh::new_rectangle(
+                    ctx,
+                    graphics::DrawMode::fill(),
+                    graphics::Rect {
+                        x,
+                        y,
+                        w: 15.0,
+                        h: 15.0,
+                    },
+                    Color::BLACK,
+                );
+                squares.push(rectangle.unwrap());
+            }
+        }
         MainState {
             emulator: emu::Emu::new(),
+            squares,
         }
     }
 }
@@ -46,8 +67,16 @@ impl EventHandler for MainState {
     }
 
     fn draw(&mut self, ctx: &mut Context) -> GameResult {
-        let mut canvas = graphics::Canvas::from_frame(ctx, Color::WHITE);
-        // Draw code here...
+        let mut canvas = Canvas::from_frame(ctx, Color::WHITE);
+        for i in 0..(64 * 32) {
+            if self.emulator.pixels[i] {
+                // pixel is turned on
+                canvas.draw(&self.squares[i], DrawParam::default().color(Color::WHITE));
+            } else {
+                // pixel is turned off
+                canvas.draw(&self.squares[i], DrawParam::default().color(Color::BLACK));
+            }
+        }
         canvas.finish(ctx)
     }
 }
