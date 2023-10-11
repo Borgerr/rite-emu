bf7fe78809ced0d511373d2877f162d88796f282
ash <>
2023-10-11 17:27:04 -0600

    doc comments

--- src/emu.rs
+++ src/emu.rs
@@ -1,3 +1,18 @@
+enum EmulationError {
+    StackOverflow,
+}
+
+/// Represents the actual emulation of a CHIP-8 system.
+///
+/// # Fields
+/// * `pixels` - public field representing on/off data for the screen's pixels
+/// * `the_stack` - stack for 16-bit addresses
+/// * `memory` - 4 kB of memory, represented as a `Vec<u8>`
+/// * `pc` - the program counter, decodes to current instruction in memory
+/// * `i` - index register, points at various locations in memory
+/// * `delay_timer` - weird delay thing that CHIP-8 programs use
+/// * `sound_timer` - like `delay_timer` but for sound
+/// * `variables` - 16 one byte variable registers.
 pub struct Emu {
     pub pixels: Vec<bool>, // true if on, false if off.
     the_stack: Vec<u16>,   // stack for 16-bit addresses
@@ -7,10 +22,15 @@ pub struct Emu {
     delay_timer: u8,       // decremented at a rate of 60 Hz until it reaches zero
     sound_timer: u8,       // functions like the delay timer, but gives a beeping noise
     // as long as it isn't 0
-    variables: Vec<u8>, // 16 variable registers
+    variables: Vec<u8>, // 16 variable registers- could be represented instead with 0 - F.
+                        // F (the last register) is used as a flag register,
+                        // i.e. instructions may set it to 1 or 0 from some rule.
 }
 
 impl Emu {
+    /// returns an instance of Emu.
+    /// Everything is initialized to the basic emulation environment,
+    /// but without the actual program.
     pub fn new() -> Self {
         let mut memory: Vec<u8> = vec![0; 4096];
         // font stuff. this is a LOT of hex,
@@ -49,9 +69,52 @@ impl Emu {
             // maybe change these later VVV
             pc: 0,
             i: 0,
-            delay_timer: 0,
-            sound_timer: 0,
+            delay_timer: 255,
+            sound_timer: 255,
             variables: vec![0; 16], // should always have only 16 elements
+        }
+    }
+
+    /// Pushing to the stack with the mandate of a 16 entry limit
+    ///
+    /// # Arguments:
+    /// * self
+    /// * `entry` - 16-bit entry to be placed on the stack. Should be something like an address.
+    fn stack_push(&mut self, entry: u16) -> Result<(), EmulationError> {
+        self.the_stack.push(entry);
+        if self.the_stack.len() > 16 {
+            Err(EmulationError::StackOverflow)
+        } else {
+            Ok(())
+        }
+    }
+    /// Pops from the stack.
+    /// I just think this is nicer within the context of OOP,
+    /// given the presence of `stack_push`.
+    fn stack_pop(&mut self) -> u16 {
+        if let Some(val) = self.the_stack.pop() {
+            val
+        } else {
+            0 // maybe revisit this alternative return value
+        }
+    }
+
+    /// Decrements `delay_timer`,
+    /// should be accessed by `main.rs` during the main loop
+    pub fn decrement_delay(&mut self) {
+        if self.delay_timer > 0 {
+            self.delay_timer -= 1;
+        } else {
+            self.delay_timer = 255;
+        }
+    }
+    /// Decrements `sound_timer`,
+    /// should be accessed by `main.rs` during the main loop
+    pub fn decrement_sound(&mut self) {
+        if self.sound_timer > 0 {
+            self.sound_timer -= 1;
+        } else {
+            self.delay_timer = 255;
         }
     }
 }
